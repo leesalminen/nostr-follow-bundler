1d0fd8e38828eca5573ebf8394a74dc73ae157b1
Lee Salminen <leesalminen@gmail.com>
2023-02-05 12:47:13 -0600

    better relay management

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import { useEffect, useState } from 'react'
 import { NostrProvider} from "nostr-react"
+import { relayInit } from "nostr-tools"
 import LoggedIn from './LoggedIn'
 
 import './App.css';
 
+const defaultRelayUrls = [
+  'wss://no.str.cr',
+  'wss://paid.no.str.cr',
+  'wss://nostr.fly.dev',
+  'wss://nostr.robotechy.com',
+  'wss://nostr-relay.untethr.me',
+  'wss://nostr-pub.wellorder.net',
+  'wss://nostr.bitcoiner.social',
+  'wss://nostr.fmt.wiz.biz',
+  'wss://nostr-relay.wlvs.space',
+  'wss://relay.snort.social',
+  'wss://nostr.fly.dev',
+  'wss://relay.realsearch.cc',
+  'wss://relay.nostrgraph.net',
+  'wss://relay.minds.com/nostr/v1/ws',
+  'wss://nos.lol',
+  'wss://relay.current.fyi',
+  'wss://brb.io',
+  'wss://puravida.nostr.land',
+  'wss://nostr.milou.lol',
+]
+
 function App() {
 
+  const [mostRecentKind3, setMostRecentKind3] = useState(0)
   const [relayUrls, setRelayUrls] = useState([])
   const [pubkey, setPubkey] = useState(null)
-
 
   useEffect(() => {
     async function testNos2x() {
@@ -28,6 +51,42 @@ function App() {
 
     setTimeout(testNos2x, 500)
   }, []);
+
+  useEffect(() => {
+    async function getRelayList() {
+      defaultRelayUrls.forEach(async (relayUrl) => {
+        const relay = relayInit(relayUrl)
+        await relay.connect()
+
+        relay.on('connect', () => {
+          const sub = relay.sub([
+            {
+              authors: [pubkey],
+              kinds: [3],
+            }
+          ])
+          sub.on('event', event => {
+            if(event.created_at > mostRecentKind3) {
+              const obj = JSON.parse(event.content)
+              const relayUrls = Object.keys(obj)
+
+              setRelayUrls(relayUrls)
+              setMostRecentKind3(event.created_at)
+            }
+          })
+          sub.on('eose', () => {
+            sub.unsub()
+            relay.disconnect()
+          })
+        })
+      })
+    }
+
+    if(pubkey) {
+      getRelayList()
+    }
+    
+  }, [pubkey])
 
   const selectRelays = () => {
     let relayUrls = prompt("Enter a list of comma separated relay URLs")
